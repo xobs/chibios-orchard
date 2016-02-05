31476030d5853fca253c760597a736f0c092438f
Sean Cross <xobs@kosagi.com>
2016-02-05 16:20:50 +0800

    cmd-usbcap: add command to initiate a capture
    During testing, it's important to be able to initiate a capture
    outside of the usual wait-for-interrupt.  This command captures one
    packet, and then prints out the command buffer.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

---
+++ palawan/cmd-usbcap.c
@@ -0,0 +1,37 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006-2013 Giovanni Di Sirio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include "ch.h"
+#include "shell.h"
+#include "chprintf.h"
+
+#include "palawan-shell.h"
+
+extern void usbStateTransitionI(void);
+extern void cmd_usbla(BaseSequentialStream *chp, int argc, char *argv[]);
+
+void cmd_usbcap(BaseSequentialStream *chp, int argc, char *argv[])
+{
+	chprintf(chp, "Sampling USB pins... ");
+	chSysLock();
+	usbStateTransitionI();
+	chSysUnlock();
+	chprintf(chp, "Done.\r\n");
+
+	cmd_usbla(chp, argc, argv);
+}
+
+palawan_command("usbcap", cmd_usbcap);

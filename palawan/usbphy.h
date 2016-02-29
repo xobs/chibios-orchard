837fd3d668b9e1f66b3f0cd4c86cf3e61a3b4dd1
Sean Cross <xobs@kosagi.com>
2016-02-29 17:54:24 +0800

    usbphy: remove unused values from stats struct
    We don't keep an internal buffer anymore, as it's taken care of in
    realtime by the low-level code.  So there are no more buffer
    statistics to display.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- palawan/usbphy.h
+++ palawan/usbphy.h
@@ -8,14 +8,10 @@ struct USBPHY;
 struct usb_phy_statistics {
   int num_packets;
   int errors;
-  int underflow;
   int overflow;
   int timeout;
   int no_end_of_sync;
   int no_end_of_frame;
-  int in_read_head;
-  int in_write_head;
-  int in_buffer_size;
   int out_read_head;
   int out_write_head;
   int out_buffer_size;
@@ -29,7 +25,10 @@ int usbPhyResetStatistics(void);
 void usbInit(void);
 int usbPhyRead(const struct USBPHY *phy, uint8_t samples[11], uint32_t scratch[3]);
 int usbPhyWrite(const struct USBPHY *phy, const uint8_t samples[11], uint32_t count);
+void usbPhyWriteTestPattern(const struct USBPHY *phy);
+void usbPhyWriteTest(void);
 int usbProcessIncoming(void);
 int usbPhyQueue(const uint8_t *buffer, int buffer_size);
+int usbCapture(uint8_t samples[11]);
 
 #endif /* __USB_PHY_H__ */

79f4d4a805a6644800c827aa09c98838964ae722
Sean Cross <xobs@kosagi.com>
2016-02-29 18:00:32 +0800

    usbphy_ll: completely rework to do everything inline
    This does bit conversion and unstuffing inline.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- palawan/usbphy_ll.s
+++ palawan/usbphy_ll.s
@@ -1,6 +1,6 @@
-.text
+.section .text    /* Can also run out of .section .ramtext */
 
-usbphy  .req r0   /* Pointer to the USBPHY struct, described above */
+usbphy  .req sp   /* Pointer to the USBPHY struct, described above */
 outptr  .req r1   /* Outgoing sample buffer (pushed to stack) */
 
 one     .req r1   /* The value 1 */
@@ -13,10 +13,8 @@ sample2 .req r6   /* Next 32-bits */
 sample3 .req r7   /* Remaining 24-bits */
 
 lastval .req r8   /* What value was the last pin? */
-usbphybkp .req r9
 counter .req r10
 unstuff .req r11
-tmp     .req r0
 
 #if 0
   /***************************************************************************
@@ -56,19 +54,6 @@ tmp     .req r0
    */
 #endif
 
-wait_16_cycles: nop
-wait_15_cycles: nop
-wait_14_cycles: nop
-wait_13_cycles: nop
-wait_12_cycles: nop
-wait_11_cycles: nop
-wait_10_cycles: nop
-wait_9_cycles:  nop
-wait_8_cycles:  nop
-wait_7_cycles:  nop
-wait_6_cycles:  mov pc, lr
-/* Less than 6 cycles, call nop. */
-
 /* usbphy offsets */
 .equ dpIAddr,0x00
 .equ dpSAddr,0x04
@@ -83,6 +68,8 @@ wait_6_cycles:  mov pc, lr
 .equ dnDAddr,0x24
 .equ dnMask,0x28
 .equ dnShift,0x2c
+
+.equ spSave,0x30
 
   /*
    *
@@ -122,14 +109,13 @@ wait_6_cycles:  mov pc, lr
    * Continue reading bits in until we get a double-SE0.  Actually, any
    * SE0 should be considered end-of-frame.
    *
-   * r0
    */
 
 /* Define this to have dpIAddr and dnIAddr point to uint32_t arrays rather
  * than GPIO banks.  This is used to test the code logic (but, obviously,
  * not the code timing.)
  */
-#define USB_PHY_READ_TEST
+//#define USB_PHY_READ_TEST
 
 .func usbPhyRead
 .global usbPhyRead
@@ -137,25 +123,34 @@ wait_6_cycles:  mov pc, lr
   push {r4, r5, r6, r7, lr}
   push {outptr, r2}
 
+  // We need an extra lo register for the unstuff pattern #0b111111, but
+  // we're all out.  As a complete and total hack, re-use the stack pointer
+  // to index into the USBPHY struct.
+  // This means that we can't push anything onto the stack, nor can we call
+  // any functions that do.
+  mov r2, sp
+  str r2, [r0, #spSave]
+  mov usbphy, r0
+
   /* Clear out the register shift-chain */
   mov sample1, #0
   mov sample2, #0
   mov sample3, #0
   mov counter, sample1
-  mov usbphybkp, usbphy
-  mov one, #0b11
-  mov unstuff, one                // Load 0b11 into unstuff reg, as the header
+  mov r0, #0b11
+  mov unstuff, r0                 // Load 0b11 into unstuff reg, as the header
                                   // ends with the pattern KK, which starts
                                   // a run of two.
-  mov one, #1
+  mov r0, #0b111111               // Unstuff mask
+  mov one, #1                     // Actually load the value '1' into the reg.
 
-  ldr reg, [usbphy, #dpIAddr]     // load the gpio register into r3
-  ldr mash, [usbphy, #dpMask]     // Mask to get USB line bit from register
+  ldr reg, [usbphy, #dpIAddr]     // Grab the address for the data input reg.
+  ldr mash, [usbphy, #dpMask]     // Grab the mask for the bit.
 
   /* Wait for the line to flip */
-  ldr val, [reg]              // Sample USBDP
-  and val, val, mash      // Mask off the interesting bit
-  mov lastval, val
+  ldr val, [reg]                  // Sample D+, to watch for it flipping
+  and val, val, mash              // Mask off the interesting bit
+  mov lastval, val                // Save the bit for use in looking for sync
 
 #if defined(USB_PHY_READ_TEST)
   /* Advance to the next sample */
@@ -173,26 +168,29 @@ wait_6_cycles:  mov pc, lr
   ldr reg, [usbphy, #dpIAddr]
 #endif /* defined(USB_PHY_READ_TEST) */
 
-  // The loop is 4 cycles on a failure.  One
+  // The loop is 5 cycles on a failure.  One
   // pulse is 32 cycles.  Therefore, loop up
-  // to 9 times before giving up.
+  // to 8 times before giving up.
 #if !defined(USB_PHY_READ_TEST)
-.rept 11
+.rept 8
   ldr val, [reg]                  // Sample USBDP
   and val, val, mash              // Mask off the interesting bit
   cmp val, lastval                // Wait for it to change
-  bne usb_phy_read_sync_wait
+  bne usb_phy_read_sync_wait      // When it changes, go wait for sync pulse
 .endr
-  b usb_phy_read_timeout
+  b usb_phy_read_timeout          // It never changed, so return "timeout".
 #endif
 
 usb_phy_read_sync_wait:
-  // There should be about 16 instructions between "ldr" above and "ldr" below.
-  // Minus up to 8 instructions for the "beq" path
 #if !defined(USB_PHY_READ_TEST)
-  bl usb_read_wait_13_cycles
+  // Wait until we're in the middle of a pulse.  When we get here, the pulse
+  // will have happened between 6 and 10 cycles ago.  Since the middle of a
+  // pulse occurs at 16 cycles, delay 8 cycles to line us up.
+//  bl usb_read_wait_8_cycles
 #endif /* !defined(USB_PHY_READ_TEST) */
 
+  // Wait for the end-of-header sync pulse, which is when the value
+  // repeats itself.  This is the "KK" in the KJKJKJKK training sequence.
 .rept 7
   ldr val, [reg]                  // Sample USBDP
 #if defined(USB_PHY_READ_TEST)
@@ -218,33 +216,37 @@ usb_phy_read_sync_wait:
   bl usb_read_wait_27_cycles
 #endif /* defined(USB_PHY_READ_TEST) */
 .endr
-  b usb_phy_read_timeout
+  b usb_phy_sync_timeout
 
   /* We're synced to the middle of a pulse, and the clock sync / start-of-
    * -frame has been found.  Real packet data follows.
    */
 start_reading_usb:
 
-get_usb_bit:
-  ldr reg, [usbphy, #dpIAddr]     // load the gpio register into r3
-  ldr mash, [usbphy, #dpShift]    // load the gpio register into r3
-  ldr val, [reg]                  // Sample USBDP
-  mov one, #1
+  /* Adjust lastval so that it's in the correct position -- we skip doing
+     this above since we're only interested in the value changing, not in
+     what the value is.  However, we're now interested in what the value
+     is, so we now deal with shifts instead of masks, and always mask by
+     #1.
+   */
+  mov val, lastval
+  ldr mash, [usbphy, #dpShift]
+  ror val, val, mash
+  and val, val, one
+  mov lastval, val
+
+usb_phy_read_get_usb_bit:
+  ldr val, [usbphy, #dpIAddr]     // Get the address of the D+ input bank
 #if defined(USB_PHY_READ_TEST)
   /* Advance to the next sample */
+  mov reg, val
+  ldr val, [val]                  // Actually sample D+
   add reg, reg, one
   add reg, reg, one
   add reg, reg, one
   add reg, reg, one
   str reg, [usbphy, #dpIAddr]     // Move read test register up by 4
-#endif /* defined(USB_PHY_READ_TEST) */
-  ror val, val, mash              // Get it down to one bit
-  mov one, #1
-  and val, val, one               // And mask off everything else
-
-  // Check for SE0
   ldr reg, [usbphy, #dnIAddr]
-#if defined(USB_PHY_READ_TEST)
   ldr mash, [reg]
   add reg, reg, one
   add reg, reg, one
@@ -253,99 +255,105 @@ get_usb_bit:
   str reg, [usbphy, #dnIAddr]   // Move read test register up by 4
   mov reg, mash
 #else
-  ldr reg, [reg]
+  ldr reg, [usbphy, #dnIAddr]    // Get the address of the D- input bank
+  ldr val, [val]                  // Actually sample D+
+  ldr reg, [reg]                 // Also sample D-
 #endif /* defined(USB_PHY_READ_TEST) */
-  ldr mash, [usbphy, #dnShift]
-  ror reg, reg, mash
-  and reg, reg, one
-  add reg, reg, val  // An end-of-frame is indicated by two
-                                // frames of SE0.  If this is the case,
-                                // then the result of adding these together
-                                // will result in 0.
-  cmp reg, #0
-  beq exit                      // Exit if so.
-  ldr mash, [usbphy, #dpShift]
-  ldr reg, [usbphy, #dnIAddr]
-  // ??
+  ldr mash, [usbphy, #dpShift]    // Get the mask of the D+ bit
+  ror val, val, mash              // Rotate the value down to bit position 1.
+  and val, val, one               // Mask off everything else.
+  // 7 [5]
 
-  mov tmp, lastval
+  // Check for SE0
+#if 1
+  ldr mash, [usbphy, #dnMask]
+  and reg, reg, mash
+  add reg, reg, val               // An end-of-frame is indicated by two
+                                  // frames of SE0.  If this is the case,
+                                  // then the result of adding these together
+                                  // will result in 0.
+  beq usb_phy_read_exit           // Exit if so.
+#else
+  mov mash, counter
+  cmp mash, #88
+  beq usb_phy_read_exit
+#endif
+  // 4 [3]
+
+  mov mash, lastval
   mov lastval, val
-  eor val, tmp                   // Check to see if the state has flipped
-  mvn val, val                    // Invert, as the bits come across flipped
-  and val, val, one
+  eor val, mash                   // Check to see if the state has changed.
+  mvn val, val                    // Invert, as 1 ^ 1 or 0 ^ 0 should be 1.
+  and val, val, one               // Mask off everything but the last bit.
 
-  mov mash, unstuff
-  lsl mash, mash, one
-  orr mash, mash, val
-  mov unstuff, mash
+  add unstuff, unstuff, unstuff   // Shift the "unstuff" value up by 1.
+  add unstuff, unstuff, val       // "or" in the one-bit "val" to the bottom.
 
-  lsr val, val, one              // Shift the state into the carry bit
-  adc sample1, sample1, sample1  // Propagate the carry bit up
-  adc sample2, sample2, sample2  // Propagate the carry bit up
-  adc sample3, sample3, sample3  // Propagate the carry bit up
-  // 10
-
-  // Unstuff bits.  Six consecutive USB states will be followed by a dummy
-  // state flip.  Ignore this.
-  mov tmp, #0b111111              // Check to see if the next bit will be dummy
-  mov mash, unstuff
-  and mash, mash, tmp
-  cmp tmp, mash
-  beq usb_unstuff
+  lsr val, val, one               // Shift the state into the carry bit
+  adc sample1, sample1, sample1   // Propagate the carry bit up
+  adc sample2, sample2, sample2   // Propagate the carry bit up
+  adc sample3, sample3, sample3   // Propagate the carry bit up
+  // 11 [9]
 
   /* Restore values, increase counter */
-  mov one, #1                     // Restore "one"
-  mov usbphy, usbphybkp           // Restore "usbphy"
   add counter, counter, one
+  // 1
 
-
-#if !defined(USB_PHY_READ_TEST)
-  bl usb_read_wait_6_cycles
-#endif /* defined(USB_PHY_READ_TEST) */
-  b get_usb_bit
-  // 2
+  // Figure out if we need to unstuff, or if we can just continue to the
+  // next bit.
+  // Six consecutive USB states will be followed by a dummy
+  // state flip.  Ignore this.
+  mov reg, unstuff
+  and reg, reg, r0
+  cmp reg, r0
+  bne usb_phy_read_get_usb_bit
+  // 4 [0]
 
 usb_unstuff:
-  mov one, #1                     // Restore "one"
-  mov unstuff, one
-  mov usbphy, usbphybkp           // Restore "usbphy"
-  add counter, counter, one
-// Just invert the value and ignore the next bit
+  nop                               // We get here when the current bit has one
+                                    // more clock cycle left.  Add a nop just
+                                    // to make the cycle-counting easier.
+  /* --- New pulse starts here --- */
+  // NOTE that we don't increment "counter" here.
+  mov unstuff, one                  // We're skipping over one bit, which
+                                    // results in a new run of one.
+  // 1
+
 #if defined(USB_PHY_READ_TEST)
   ldr reg, [usbphy, #dnIAddr]
   add reg, reg, one
   add reg, reg, one
   add reg, reg, one
   add reg, reg, one
-  str reg, [usbphy, #dnIAddr]   // Move read test register up by 4
+  str reg, [usbphy, #dnIAddr]       // Move read test register up by 4
   ldr reg, [usbphy, #dpIAddr]
   add reg, reg, one
   add reg, reg, one
   add reg, reg, one
   add reg, reg, one
-  str reg, [usbphy, #dpIAddr]   // Move read test register up by 4
+  str reg, [usbphy, #dpIAddr]       // Move read test register up by 4
 #endif /* defined(USB_PHY_READ_TEST) */
-  mov reg, lastval
-  mvn reg, reg
-  and reg, reg, one
-  mov lastval, reg
-  // 18
+
+
+  /* Invert the last value, since a false 0 was added to the stream */
+  mov reg, lastval                  // Read the current last val into a lo reg.
+  mvn reg, reg                      // Negate the value.
+  and reg, reg, one                 // Mask it with 0b1
+  mov lastval, reg                  // Save it back into a lo reg.
+  // 4
+
 #if !defined(USB_PHY_READ_TEST)
-  bl usb_read_wait_12_cycles
+  bl usb_read_wait_23_cycles
 #endif /* defined(USB_PHY_READ_TEST) */
-  b get_usb_bit
+  b usb_phy_read_get_usb_bit
+  // 2
 
-exit:
-
-  mov one, #1
-.rept 0
-  lsr val, val, one              // Shift the state into the carry bit
-  adc sample1, sample1, sample1  // Propagate the carry bit up
-  adc sample2, sample2, sample2  // Propagate the carry bit up
-  adc sample3, sample3, sample3  // Propagate the carry bit up
-.endr
+usb_phy_read_exit:
+  ldr r2, [usbphy, #spSave]
+  mov sp, r2
   pop {outptr, r2}
 
+  /* Bits come across in the wrong endianess.  Fix that. */
   rev sample1, sample1
   rev sample2, sample2
   rev sample3, sample3
@@ -365,10 +373,20 @@ exit:
   mov retval, counter
   mov r3, #3
   asr retval, retval, r3              // Number of bytes read (return value)
+  cmp retval, #11
+  bgt usb_phy_read_overflow_exit
+
+  cmp retval, #0
+  beq usb_phy_read_no_data_exit
 
   mov srcoff, retval
   mov endoff, #12
+#if 1 // XXX testing
   sub srcoff, endoff, srcoff
+#else
+  mov srcoff, #1
+  mov retval, #11
+#endif
   mov dstoff, #0
 
 copy_loop_top:
@@ -383,15 +401,35 @@ copy_loop_top:
 final_exit:
   pop {r4,r5,r6,r7,pc}
 
+  /* Read too many bits, return -2 */
+usb_phy_read_overflow_exit:
+  mov r1, #2
+  mov r0, #0
+  sub r0, r0, r1
+  pop {r4,r5,r6,r7,pc}
+
+usb_phy_read_no_data_exit:
+  mov r0, #0
+  pop {r4,r5,r6,r7,pc}
+
+usb_phy_sync_timeout:
+  ldr r2, [usbphy, #spSave]
+  mov sp, r2
+  pop {outptr, r2}
+  mov r0, #0
+  sub r0, r0, #3
+  pop {r4,r5,r6,r7,pc}
+
 usb_phy_read_timeout:
+  ldr r2, [usbphy, #spSave]
+  mov sp, r2
   pop {outptr, r2}
   mov r0, #0
   sub r0, r0, #1
   pop {r4,r5,r6,r7,pc}
-.endfunc
-.type usbPhyRead, %function
-.size usbPhyRead, .-usbPhyRead
 
+usb_read_wait_30_cycles: nop
+usb_read_wait_29_cycles: nop
 usb_read_wait_28_cycles: nop
 usb_read_wait_27_cycles: nop
 usb_read_wait_26_cycles: nop
@@ -417,6 +455,10 @@ usb_read_wait_7_cycles:  nop
 usb_read_wait_6_cycles:  nop
 usb_read_wait_5_cycles:  mov pc, lr
 
+.endfunc
+.type usbPhyRead, %function
+.size usbPhyRead, .-usbPhyRead
+
 .global usbPhyRead_size
 .align 4
 usbPhyRead_size: .long usbPhyRead, .-usbPhyRead
@@ -432,7 +474,6 @@ usbPhyRead_size: .long usbPhyRead, .-usbPhyRead
  *   r4: dn set/clear
  *   r5: dp mask
  *   r6: dn mask
- *   r7: tmp
  */
 .func usbPhyWrite
 .global usbPhyWrite
@@ -530,8 +571,173 @@ usb_phy_commit_values:
 .endfunc
 .type usbPhyWrite, %function
 .size usbPhyWrite, .-usbPhyWrite
-.global usbPhyWrite_size
-.align 4
-usbPhyWrite_size: .long usbPhyWrite, .-usbPhyWrite
-.end
 
+
+.func usbPhyWriteTestPattern
+.global usbPhyWriteTestPattern
+usbPhyWriteTestPattern:
+  push {lr}
+
+  /* Set D+ to output */
+  ldr reg, [usbphy, #dpDAddr]     // Get the direction address
+  ldr val, [reg]                  // Get the direction value
+  ldr mash, [usbphy, #dpMask]     // Get the mask value for ORing in
+  orr val, val, mash              // Set the direciton mask
+  str val, [reg]                  // Set the direction for Dp
+
+  /* Set D- to output */
+  ldr reg, [usbphy, #dnDAddr]     // Get the direction address
+  ldr val, [reg]                  // Get the direction value
+  ldr mash, [usbphy, #dnMask]     // Get the mask value for ORing in
+  orr val, val, mash              // Set the direciton mask
+  ldr reg, [usbphy, #dnDAddr]     // Get the direction address
+  str val, [reg]                  // Set the direction for Dp
+
+  ldr r1, [r0, #dpSAddr]
+  ldr r2, [r0, #dpCAddr]
+  ldr r3, [r0, #dpMask]
+
+  ldr r4, [r0, #dnSAddr]
+  ldr r5, [r0, #dnCAddr]
+  ldr r6, [r0, #dnMask]
+
+  // usb start-of-frame header //
+  bl usb_write_state_k
+  bl usb_write_state_j
+  bl usb_write_state_k
+  bl usb_write_state_j
+  bl usb_write_state_k
+  bl usb_write_state_j
+  bl usb_write_state_k
+  bl usb_write_state_k
+  // end of header //
+
+  // 0x00
+  // 0x00
+  // 0x00
+  // 0x00
+  // 0xd5
+  // 0x6d
+  // 0xdd
+  // 0x80
+  bl usb_write_state_j // 0
+  bl usb_write_state_k // 0
+  bl usb_write_state_j // 0
+  bl usb_write_state_k // 0
+  bl usb_write_state_j // 0
+  bl usb_write_state_k // 0
+  bl usb_write_state_j // 0
+  bl usb_write_state_k // 0
+
+  bl usb_write_state_j // 0
+  bl usb_write_state_k // 0
+  bl usb_write_state_j // 0
+  bl usb_write_state_k // 0
+  bl usb_write_state_j // 0
+  bl usb_write_state_k // 0
+  bl usb_write_state_j // 0
+  bl usb_write_state_k // 0
+
+  bl usb_write_state_j // 0
+  bl usb_write_state_k // 0
+  bl usb_write_state_j // 0
+  bl usb_write_state_k // 0
+  bl usb_write_state_j // 0
+  bl usb_write_state_k // 0
+  bl usb_write_state_j // 0
+  bl usb_write_state_k // 0
+
+  bl usb_write_state_j // 0
+  bl usb_write_state_k // 0
+  bl usb_write_state_j // 0
+  bl usb_write_state_k // 0
+  bl usb_write_state_j // 0
+  bl usb_write_state_k // 0
+  bl usb_write_state_j // 0
+  bl usb_write_state_k // 0
+
+  bl usb_write_state_k // 1
+  bl usb_write_state_k // 1
+  bl usb_write_state_j // 0
+  bl usb_write_state_j // 1
+  bl usb_write_state_k // 0
+  bl usb_write_state_k // 1
+  bl usb_write_state_j // 0
+  bl usb_write_state_j // 1
+
+  bl usb_write_state_k // 0
+  bl usb_write_state_k // 1
+  bl usb_write_state_k // 1
+  bl usb_write_state_j // 0
+  bl usb_write_state_j // 1
+  bl usb_write_state_j // 1
+  bl usb_write_state_k // 0
+  bl usb_write_state_k // 1
+
+  bl usb_write_state_k // 1
+  bl usb_write_state_k // 1
+  bl usb_write_state_j // 0
+  bl usb_write_state_j // 1
+  bl usb_write_state_j // 1
+  bl usb_write_state_j // 1
+  bl usb_write_state_k // 0
+  bl usb_write_state_k // 1
+
+  bl usb_write_state_k // 1
+  bl usb_write_state_j // 0
+  bl usb_write_state_k // 0
+  bl usb_write_state_j // 0
+  bl usb_write_state_k // 0
+  bl usb_write_state_j // 0
+  bl usb_write_state_k // 0
+  bl usb_write_state_j // 0
+
+  // start of end-of-frame
+  bl usb_write_state_se0
+  bl usb_write_state_se0
+  // end of end-of-frame
+
+  pop {pc}
+
+  /* These all specify one extra cycle.  If you add up the numbers, you'll
+     find we should sleep for 26 cycles.  However, those names assume you
+     get there via a "bl", which adds an extra cycle over "b" (3 cycles
+     instead of 2.)  Therefore, branch to usb_read_wait_27_cycles instead
+     of usb_read_wait_26_cycles to compensate.
+   */
+usb_write_state_j:
+  str r3, [r1]    // D+ set
+  str r6, [r5]    // D- clr
+  b usb_read_wait_27_cycles
+
+usb_write_state_k:
+  str r3, [r2]    // D+ clr
+  str r6, [r4]    // D- set
+  b usb_read_wait_27_cycles
+
+usb_write_state_se0:
+  str r3, [r2]    // D+ clr
+  str r6, [r5]    // D- clr
+  b usb_read_wait_27_cycles
+
+.endfunc
+.type usbPhyWriteTestPattern, %function
+.size usbPhyWriteTestPattern, .-usbPhyWriteTestPattern
+
+.func asmtest
+.global asmtest
+asmtest:
+  /*int asmtest(int a1, int a2)*/
+
+  add r1, r1, r0
+  beq asmtest_equal
+  mov r0, #0
+  bx lr
+
+asmtest_equal:
+  mov r0, #1
+  bx lr
+
+.endfunc
+.type asmtest, %function
+.size asmtest, .-asmtest

a08913934debf99a8796b6b4b3058d3d37b03bec
Sean Cross <xobs@kosagi.com>
2016-04-13 18:26:39 +0800

    palawan-events: enable radio ISR
    Call the radio ISR when the right IRQ gets hit.  This will drain the radio,
    and generally get it working on Palawan.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- palawan/palawan-events.c
+++ palawan/palawan-events.c
@@ -4,29 +4,7 @@
 
 #include "usbphy.h"
 
-#if 0
-event_source_t ble_rdy;
-event_source_t rf_pkt_rdy;
-event_source_t gpiox_rdy;
-
-event_source_t celcius_rdy;
-event_source_t mic_rdy;
-event_source_t usbdet_rdy;
-
-event_source_t radio_page;
-event_source_t radio_sex;
-event_source_t radio_app;
-
-static void ble_rdyn_cb(EXTDriver *extp, expchannel_t channel) {
-
-  (void)extp;
-  (void)channel;
-
-  chSysLockFromISR();
-  chEvtBroadcastI(&ble_rdy);
-  chSysUnlockFromISR();
-}
-#endif
+extern void radioInterrupt(EXTDriver *extp, expchannel_t channel);
 
 void usb_state_transition_cb(EXTDriver *extp, expchannel_t channel) {
 
@@ -38,6 +16,7 @@ void usb_state_transition_cb(EXTDriver *extp, expchannel_t channel) {
 static const EXTConfig ext_config = {
   {
     {EXT_CH_MODE_BOTH_EDGES | EXT_CH_MODE_AUTOSTART, usb_state_transition_cb, PORTA, 4},
+    {EXT_CH_MODE_RISING_EDGE | EXT_CH_MODE_AUTOSTART, radioInterrupt, PORTC, 4},
   }
 };
 
